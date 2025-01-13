5aed5edc1ed7e87185a0ff29376d55dc9ce28119
AlexanderPrentiss <119530838+AlexanderPrentiss@users.noreply.github.com>
2025-01-12 23:49:33 -0500

    Added a ton of core functionality. will comment later. TODOS: clear memory, fix shuffle, fix two pair

--- poker.c
+++ poker.c
@@ -16,7 +16,8 @@
 #endif
 
 typedef enum {Clubs, Diamonds, Spades, Hearts} Suit; // mostly for comparing hands
-typedef enum {Ace, Joker=11, Queen, King} Value;
+typedef enum {Joker=11, Queen, King, Ace} Value; 
+typedef enum {high, pair = 14, three_of_a_kind, straight, flush, full_house, four_of_a_kind, straight_flush, royal_flush} Hand;
 
 typedef struct card // card contains a suit and a value 
 {
@@ -109,14 +110,152 @@ Card* deal_river(Deck* deck) {
   return river;
 }
 
+//check for flush
+int check_flush(Card* hand[HAND_SIZE])
+{
+  Suit test = hand[0]->suit;
+  for (int i = 1; i < HAND_SIZE; i++)
+  {
+    if (hand[i]->suit != test)
+    {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+// check for straight
+int check_straight(Card* hand[HAND_SIZE]) {
+  Card* hashmap[NUM_VALUES];
+  for (int i = 0; i < NUM_VALUES; i++)
+  {
+    hashmap[i] = NULL;
+  }
+  for (int i = 0; i < HAND_SIZE; i++)
+  {
+    hashmap[hand[i]->value] = hand[i];
+  }
+  int start_index;
+  for (int i = 0; i < NUM_VALUES; i++)
+  {
+    if (hashmap[i] != NULL) start_index = i;
+  }
+  for (int i = start_index; i < NUM_VALUES; i++)
+  {
+    if (hashmap[i] == NULL)
+    {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+// check for pair / three / four
+
+int check_multi(Card* hand[HAND_SIZE]) {
+  int hashmap[NUM_VALUES];
+  int count = 0;
+  for (int i = 0; i < NUM_VALUES; i++)
+  {
+    hashmap[i] = 0;
+  }
+  for (int i = 0; i < HAND_SIZE; i++)
+  {
+    hashmap[hand[i]->value]++;
+  }
+  for (int i = 0; i < NUM_VALUES; i++)
+  {
+    if (hashmap[i] > 1)
+    {
+      count += hashmap[i];
+    }
+  }
+  return count;
+}
+
+int check_highest(Card* hand[HAND_SIZE]){
+  int highest = hand[0]->value;
+  for (int i = 1; i < HAND_SIZE; i++)
+  {
+    if (hand[i]->value > highest) highest = hand[i]->value;
+  }
+  return highest;
+}
+
+//TODO evaluate hand will check to see what score a hand will get
+int evaluate_hand(Card* hand[HAND_SIZE]) {
+  if (check_flush(hand) && check_straight(hand)) return straight_flush;
+  else if (check_flush(hand)) return flush;
+  else if (check_straight(hand)) return straight;
+  else 
+  {
+    switch (check_multi(hand))
+    {
+      case 0:
+        return check_highest(hand);
+        break;
+      case 2:
+        return pair;
+        break;
+      case 3:
+        return three_of_a_kind;
+        break;
+      case 4:
+        return four_of_a_kind;
+        break;
+      case 5:
+        return full_house;
+        break;
+    }
+  }
+  return check_highest(hand);
+}
+
+//TODO set hand will set a players hand as the highest scoring hand
+void set_hand(Player* player, Card* table[HAND_SIZE]){
+  int highest = 0;
+  Card* cur_hand[HAND_SIZE];
+  Card* highest_hand[HAND_SIZE];
+  cur_hand[0] = player->pocket[0];
+  cur_hand[1] = player->pocket[1];
+  for (int i = 0; i < HAND_SIZE; i++)
+  {
+    cur_hand[2] = table[i];
+    for (int j = 0; j < HAND_SIZE; j++)
+    {
+      if (j != i)
+      {
+        cur_hand[3] = table[j];
+        for (int x = 0; x < HAND_SIZE; x++)
+        {
+          if (x!=j && x!=i)
+          {
+            cur_hand[4] = table[x];
+            if (evaluate_hand(cur_hand) > highest)
+            {
+              
+              for (int z = 0; z < HAND_SIZE; z++)
+              {
+                player->hand[z] = (Card*) malloc(sizeof(Card));
+                player->hand[z] = cur_hand[z];
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+
 //TODO destroy deck
 
-void play_hand() {
+// Test 1
+void test_deck() {
   Deck* deck = (Deck*) malloc(sizeof(Deck)); //create deck 
   Player* player = (Player*) malloc(sizeof(Player)); // create two players, player & computer
   Player* computer = (Player*) malloc(sizeof(Player));
   initialize_deck(deck); // create the deck
-  shuffle_deck(deck); // shuffle
   
   deal_cards(player, deck); // give both player cards
   deal_cards(computer, deck); 
@@ -131,7 +270,44 @@ void play_hand() {
   table[4] = *deal_river(deck);
 }
 
+// Test 2
+void test_evaluations() {
+  Deck* deck = (Deck*) malloc(sizeof(Deck)); //create deck 
+  Player* player = (Player*) malloc(sizeof(Player)); // create player
+  initialize_deck(deck);
+  shuffle_deck(deck);
+  
+  deal_cards(player, deck);
+  for (int i = 0; i < POCKET_SIZE; i++)
+  {
+    assert(player->pocket[i] != NULL);
+    printf("Player Hand: %d %d\n", player->pocket[i]->suit, player->pocket[i]->value);
+  }
+
+  Card* table[HAND_SIZE];
+  Card* flop = deal_flop(deck);
+  for (int i = 0; i < FLOP_SIZE; i++)
+  {
+    table[i] = &flop[i];
+  }
+  table[3] = deal_turn(deck);
+  table[4] = deal_river(deck);
+  
+  for (int i = 0; i < HAND_SIZE; i++)
+  {
+    printf("Table: %d, %d\n", table[i]->suit, table[i]->value);
+  }
+  
+  set_hand(player, table);
+  for (int i = 0; i < HAND_SIZE; i++)
+  {
+    printf("Best Hand: %d, %d\n", player->hand[i]->suit, player->hand[i]->value);
+  }
+
+  printf("Final score: %d\n", evaluate_hand(player->hand));
+}
+
 int main() {
-    play_hand();
-    return 0;
+  test_evaluations();
+  return 0;
 }
